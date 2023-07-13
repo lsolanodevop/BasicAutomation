8b350142e78a10f9a159ceeb54277ffcb58400eb
Leonardo Solano <leonardo.solano.hw@gmail.com>
2023-07-13 11:17:27 -0300

    [ADD] Using the correct website implemented a quick test

--- src/pages/main.page.ts
+++ src/pages/main.page.ts
@@ -3,13 +3,27 @@ import Wrapper from "../base/Wrapper";
 
 export default class MainPage extends Wrapper {
 
+    readonly usernameInput: Locator; //locator('[data-test="username"]')
+    readonly passwordInput: Locator; //locator('[data-test="password"]')
+    readonly loginButton: Locator; //locator('[data-test="login-button"]')
+    readonly errorMessage: Locator; //locator('[data-test="error"]')
+    readonly productsCategory: Locator; //getByText('Products')
+
     constructor(public page: Page){
         super(page);
+        this.usernameInput = this.page.locator('[data-test="username"]');
+        this.passwordInput = this.page.locator('[data-test="password"]');
+        this.loginButton = this.page.locator('[data-test="login-button"]');
+        this.productsCategory = this.page.getByText('Products');
     }
 
-    async checkURL(){
+    async loginAsUser(username: string){
+        await this.usernameInput.fill(username);
+        await this.passwordInput.fill('secret_sauce');
+        await this.loginButton.click();
+        expect(await this.productsCategory.isVisible()).toBeTruthy();
         const pageURL = await this.page.url();
-
-        expect(pageURL).toBe('https://www.google.com/');
+        expect(pageURL).toContain('inventory.html');
     }
+
 }
--- src/tests/main.spec.ts
+++ src/tests/main.spec.ts
@@ -1,12 +1,21 @@
 import test from '../utils/fixtures';
 
-test.describe('Main page', async () => {
+test.describe('Sauce Demo Tests', async () => {
     test.beforeEach(async ({page}) => {
-        await page.goto('https://www.google.com');
+        await page.goto('https://www.saucedemo.com/');
     });
 
-    test('should have the correct title', async ({mainPage}) => {
-        await mainPage.checkURL();
+    test.only('Should be able to login with a standard user', async ({mainPage}) => {
+        await mainPage.loginAsUser('standard_user');
+    });
+    test('Should be able to login with a locked_out_user user', async ({mainPage}) => {
+        await mainPage.loginAsUser('locked_out_user');
+    });
+    test('Should be able to login with a problem_user', async ({mainPage}) => {
+        await mainPage.loginAsUser('problem_user');
+    });
+    test('Should be able to login with a performance_glitch_user', async ({mainPage}) => {
+        await mainPage.loginAsUser('performance_glitch_user');
     });
 });
 
